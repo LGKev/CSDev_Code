8b6f06490a17a176f3d73f788f559803eb882673
Marissa Bueno <marissa.bueno21@gmail.com>
2018-11-07 20:52:58 -0700

    added conversion & put ejs back in

--- Marissa/server.js
+++ Marissa/server.js
@@ -1,22 +1,53 @@
-let request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+
 require('dotenv').load();
 
-
 let apiKey = process.env.MY_API_KEY;
-let city = 'boulder';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
 
 
+app.post('/', function (req, res, ) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  
 
+  request(url, function (err, response, body ) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let temp = `${weather.main.temp}`;
+        console.log(temp);
+        conversion(temp);
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
 
-request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
+function conversion(temp) {
+  if(temp>70){
+    console.log('hot')
   } else {
-    let weather = JSON.parse(body)
-    let temp = `${weather.main.temp}`;
-    console.log(temp);
-    
+    console.log('cold')
   }
-});
+}
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
