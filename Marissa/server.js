e43871d7385020e931745a5a9177e49e3dd3af14
Marissa Bueno <marissa.bueno21@gmail.com>
2018-11-08 18:06:27 -0700

    transfer

--- Marissa/server.js
+++ Marissa/server.js
@@ -5,53 +5,24 @@ const app = express()
 
 
 require('dotenv').load();
-var dt = require('./modules/buenoDate');
-var weather = require('./modules/test');
 
 let apiKey = process.env.MY_API_KEY;
 
-//api
-
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
 
-app.get('/testing', function (req, res) {
-  res.render('index', {weather: null, error: null});
-})
 
-app.post('/testing',function (req, res) {
-  let city = req.body.city;
-  console.log(city);
-  res.write("Test: " + weather.test(city));
-  res.end();
-})
-
-app.get('/date', function (req, res) {
-  res.write("The date and time are currently: " + dt.buenoDate());
-  res.end();
-})
-
-app.get('/pup', function (req, res) {
-  res.render('pup', {pup: "NEWF"})
-  res.end();
-})
-
-
-
-
-
-
-
-app.post('/', function (req, res) {
+app.post('/', function (req, res, ) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  
 
-  request(url, function (err, response, body, ) {
+  request(url, function (err, response, body ) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
@@ -60,12 +31,22 @@ app.post('/', function (req, res) {
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let temp = `${weather.main.temp}`;
+        console.log(temp);
+        conversion(temp);
         res.render('index', {weather: weatherText, error: null});
       }
     }
   });
 })
 
+function conversion(temp) {
+  if(temp>70){
+    console.log('hot')
+  } else {
+    console.log('cold')
+  }
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
