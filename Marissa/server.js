72308c0d5d62527bb57a56b66c9c17f2585b9379
Marissa Bueno <marissa.bueno21@gmail.com>
2018-11-29 19:28:52 -0700

    database

--- Marissa/server.js
+++ Marissa/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
+var db = require('database');
+module.exports = app;
+
 
 
 require('dotenv').load();
@@ -14,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null, temp: null});
+  res.render('index', {weather: null, error: null, temp: null, playlist: null});
 })
 
 
@@ -25,20 +28,40 @@ app.post('/', function (req, res, ) {
 
   request(url, function (err, response, body ) {
     if(err){
-      res.render('index', {weather: null, error: 'Error, please try again', temp: null});
+      res.render('index', {weather: null, error: 'Error, please try again', temp: null, playlist: null});
     } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again', temp: null});
+        res.render('index', {weather: null, error: 'Error, please try again', temp: null, playlist: null});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
         let temp = `${weather.main.temp}`;
         console.log(temp);
         conversion(temp);
-        res.render('index', {weather: weatherText, error: null, temp: temp});
+        //playlistURL = "5dN1bcsHyvBhViD2ANdYn9";
+        //res.render('index', {weather: weatherText, error: null, temp: temp, playlist: playlistURL});
       }
     }
   });
+  var query = 'SELECT playlist FROM cold ORDER BY RAND() LIMIT 1';
+
+    db.any(query)
+      .then(function (row) {
+          response.render('index', {
+            weather: weatherText, 
+            error: null, 
+            temp: temp, 
+            playlist: row.playlist })
+      })
+      .catch(function (err) {
+          // display error message in case an error
+          request.flash('error', err);
+          response.render('store/list', {
+            weather: null, 
+            error: "error", 
+            temp: null, 
+            playlist: null })
+      })
 })
 
 function conversion(temp) {
