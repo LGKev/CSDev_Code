6b76c2decffef0bb44b03551166345cb92a6ff59
Maggie Rzepiennik <maggierzepiennik@Maggies-MacBook-Air.local>
2018-12-18 12:46:33 -0700

    Mergeer maybe

--- database.js
+++ database.js
@@ -1,13 +1,17 @@
 var pgp = require('pg-promise')();
 
 const dbConfig = {
-   host: 'localhost',
-   port: 5431,
-   database: 'weather',
-   user: 'postgres',
-   password: '' // Fill in your PostgreSQL password here. Use empty string
+   user: 'sysadmin',
+   host: '127.0.0.1',
+   port: '5432',
+   database: 'weather2', //idk why i couldn't create weatherdatabase with sysadmin role
+   password: '12345' // Fill in your PostgreSQL password here. Use empty string
                 // if you did not set a password
 };
+
+//create user sysadmin with encrypted password '12345';
+// within postgres
+
 
 var db = pgp(dbConfig);
 
